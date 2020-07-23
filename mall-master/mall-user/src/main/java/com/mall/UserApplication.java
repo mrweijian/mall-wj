9bfb8dfe93530fb09a82dcde722b2a9cc5b30579
weijian <102760118@qq.com>
2020-07-23 17:20:55 +0800

    mall

--- mall-master/mall-user/src/main/java/com/mall/UserApplication.java
+++ mall-master/mall-user/src/main/java/com/mall/UserApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-//@EnableConfigServer
+@EnableConfigServer
 public class UserApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class);
